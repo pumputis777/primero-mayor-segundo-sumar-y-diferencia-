1937efb9f32b0552449dcd1fd7f088cc99e47f89
pumputis777 <facundopumputis@gmail.com>
2025-05-07 23:06:36 -0300

    main (7).cpp

--- main (7).cpp
+++ main (7).cpp
@@ -11,25 +11,25 @@ using namespace std;
 int main()
 {
     // Declaración de las variables
-    float num1;
-    float num2;
+    float num1; // Declaro la primera variable de tipo float
+    float num2; // Declaro la segunda variable de tipo float
 
-    // Solicitar la carga de dos números
+    // Solicita al usuario que ingrese dos numeros
     cout << "Ingrese el primer número: ";
-    cin >> num1;
+    cin >> num1; // guarda el valor dado por el usuario en la variable num1
     cout << "Ingrese el segundo número: ";
-    cin >> num2;
+    cin >> num2; // guarda el valor dado por el usuario en la variable num2
 
     // Verificar si el primer número es mayor que el segundo
-    if (num1 > num2)
+    if (num1 > num2) // Verifica si el primer número es mayor que el segundo
     {
-        // Si es mayor, mostrar la suma y la diferencia
+        // Si cumple la condicion muestra la suma y la diferencia
         cout << "La suma de los dos números es: " << num1 + num2 << endl;
         cout << "La diferencia del primero respecto al segundo es: " << num1 - num2 << endl;
     }
-    else
+    else // si no cumple la funcion ejecuta lo siguiente
     {
-        // Si no es mayor, mostrar el producto y la división
+        // como no se cumplio la funcion se muestra el producto y la division
         cout << "El producto de los dos números es: " << num1 * num2 << endl;
         cout << "La división del primero respecto al segundo es: " << num1 / num2 << endl;
     }
